b91f42172e1df50c886706a73befcdba86ac620d
mdswaley <mdswaley075@gmail.com>
2025-04-20 13:23:01 +0530

    create simple project and use of @Cacheable

---
+++ src/main/java/com/example/cachingproject/repositories/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.cachingproject.repositories;
+
+
+import com.example.cachingproject.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByEmail(String email);
+}
---
+++ src/test/java/com/example/cachingproject/CachingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cachingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
